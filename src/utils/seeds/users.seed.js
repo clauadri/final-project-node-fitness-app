902716c2ce55f65cec430b2a3b57dfa351ef9448
Kind <alvaromoreno21197@gmail.com>
2022-12-01 15:00:42 +0100

    FEAT user seeds added

--- src/utils/seeds/users.seed.js
+++ src/utils/seeds/users.seed.js
@@ -4,13 +4,44 @@ const User = require('../../api/user/user.model')
 
 const users = [
     {
-        name: 'Mister Pruebas',
-        user: 'Admin',
-        email: 'admin@gmail.com',
+        name: 'Admin Adri',
+        user: 'AdminAdri',
+        email: 'adminadri@gmail.com',
         password: 'administrator123',
         age: 20,
         rol: 'admin'
     },
+    {
+        name: 'Admin Guille',
+        user: 'AdminGuille',
+        email: 'adminguille@gmail.com',
+        password: 'administrator123',
+        age: 20,
+        rol: 'admin'
+    },
+    {
+        name: 'Admin Angelica',
+        user: 'AdminAngelica',
+        email: 'adminangelica@gmail.com',
+        password: 'administrator123',
+        age: 20,
+        rol: 'admin'
+    },
+    {
+        name: 'Admin Alvaro',
+        user: 'AdminAlvaro',
+        email: 'adminalvaro@gmail.com',
+        password: 'administrator123',
+        age: 20,
+        rol: 'admin'
+    },{
+        name: 'Default User',
+        user: 'defaultuser',
+        email: 'defaultuser@gmail.com',
+        password: 'defaultuser123',
+        age: 20,
+    },
+
 ]
 
 const usersDocuments = users.map((user) => new User(user));
