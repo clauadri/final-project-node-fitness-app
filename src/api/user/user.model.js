dd9cd6f75ac9580bcd54e8293c05f78f17a1496d
clauadri <adripa1111@gmail.com>
2022-12-01 11:26:49 +0100

    auth, new routes and user model

---
+++ src/api/user/user.model.js
@@ -0,0 +1,26 @@
+const moongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const Schema = moongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    user: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true, trim: true },
+    age: { type: String, required: true, trim: true},
+    rol: {type: String, enum: ["admin", "user"], default: "user"}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+const User = moongoose.model("users", userSchema);
+module.exports = User;
--- src/api/workouts/exercises/exercises.routes.js
+++ src/api/workouts/exercises/exercises.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Exercise = require('./exercises.model')
-
+const Exercise = require('./exercises.model');
+const upload = require("../../../middleware/file");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,6 +9,40 @@ router.get("/", async (req, res) => {
     return res.status(200).json(allExercises);
   } catch (error) {
     return res.status(500).json("Error getting exercises", error);
+  }
+});
+router.get("/:id",  async (req, res) => {
+  try {
+    const id = req.params.id;
+    const exerciseById = await Exercise.findById(id);
+    return res.status(200).json(exerciseById);
+  } catch (error) {
+    return res.status(500).json("No se a podido encontrar el ejercicio por id", error);
+  }
+});
+router.get("/:name",  async (req, res) => {
+  try {
+    const name = req.params.name;
+    const exerciseByName = await Exercise.findOne({name: name});
+    return res.status(200).json(exerciseByName);
+  } catch (error) {
+    return res.status(500).json("No se a podido encontrar el ejercicio por nombre", error);
+  }
+});
+router.post("/create", upload.single("img"), async (req, res) => {
+  try {
+    const exercise = req.body;
+    if(req.file){
+      exercise.img = req.file.path;
+    }
+     console.log(req.body);
+    const newExercise = new Exercise(exercise);
+    console.log(newExercise);
+    const createdExercise = await newExercise.save();
+    console.log(createdExercise)
+    return res.status(201).json(createdExercise);
+  } catch (error) {
+    return "Error al crear el ejercicio", error;
   }
 });
 
